d148d477541980b36b18725d6a2f409bbaac91cf
Jaloladdin-Kushnazarov <jalol2622@gmail.com>
2025-04-05 00:32:01 +0500

    reset main brench

--- src/main/java/org/example/spbootpr1/Spbootpr1Application.java
+++ src/main/java/org/example/spbootpr1/Spbootpr1Application.java
@@ -1,12 +1,9 @@
 package org.example.spbootpr1;
 
-import org.example.spbootpr1.entity.Mail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(Mail.class)
 public class Spbootpr1Application {
 
     public static void main(String[] args) {
