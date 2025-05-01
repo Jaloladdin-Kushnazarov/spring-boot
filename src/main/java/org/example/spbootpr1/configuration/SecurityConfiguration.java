f6b42eeb826f4a8864dcafaa03ddec8e82a81c8e
Jaloladdin-Kushnazarov <jalol2622@gmail.com>
2025-05-01 23:14:35 +0500

    spring boot start security JWT authenification worked simple level last

--- src/main/java/org/example/spbootpr1/configuration/SecurityConfiguration.java
+++ src/main/java/org/example/spbootpr1/configuration/SecurityConfiguration.java
@@ -8,7 +8,6 @@ import org.example.spbootpr1.dto.AppErrorDTO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
