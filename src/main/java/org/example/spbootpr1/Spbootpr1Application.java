e0f852795148af84b1790cf4600375463bb2208c
Jaloladdin-Kushnazarov <jalol2622@gmail.com>
2025-05-01 23:18:38 +0500

    spring boot start security JWT authenification worked simple level last

--- src/main/java/org/example/spbootpr1/Spbootpr1Application.java
+++ src/main/java/org/example/spbootpr1/Spbootpr1Application.java
@@ -9,8 +9,8 @@ public class Spbootpr1Application {
     public static void main(String[] args) {
         SpringApplication.run(Spbootpr1Application.class, args);
 
-        System.out.println("finished spring secruty with basic authentication");
-        System.out.println("strt spring security with JWT authentication");
+        System.out.println("finished spring secruty with JWT authentication");
+
     }
 
 }
