5b3e3f1f8c36e7a4ec74d243f30dcfb004cce38e
Jaloladdin-Kushnazarov <jalol2622@gmail.com>
2025-05-26 15:33:13 +0500

    MapStruct start

--- src/main/java/org/example/spbootpr1/Spbootpr1Application.java
+++ src/main/java/org/example/spbootpr1/Spbootpr1Application.java
@@ -9,7 +9,7 @@ public class Spbootpr1Application {
     public static void main(String[] args) {
         SpringApplication.run(Spbootpr1Application.class, args);
 
-        System.out.println("hozir spring securityni chuquroq o'rganyapman qanday ishlashi qanday protselar bo'liushini");
+        System.out.println("Spring secruty tugadi MapStruct boshladim");
 
     }
 
