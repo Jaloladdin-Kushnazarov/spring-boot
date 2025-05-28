cf6d5c47a085cdc618bbde4d574b576dadc8e234
Jaloladdin-Kushnazarov <jalol2622@gmail.com>
2025-05-28 17:53:20 +0500

    properties fayl in springboot push finished

--- src/main/java/org/example/spbootpr1/Spbootpr1Application.java
+++ src/main/java/org/example/spbootpr1/Spbootpr1Application.java
@@ -9,7 +9,7 @@ public class Spbootpr1Application {
     public static void main(String[] args) {
         SpringApplication.run(Spbootpr1Application.class, args);
 
-        System.out.println("Spring bootda MapStruct tugadi  ");
+        System.out.println("Spring bootda learning properties fayl tugadi  ");
 
     }
 
