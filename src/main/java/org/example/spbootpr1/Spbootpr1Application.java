18c994185a55357c761714bb49e4ed2d95a3a753
Jaloladdin-Kushnazarov <jalol2622@gmail.com>
2025-06-01 14:49:43 +0500

    task_schedeling in springboot START

--- src/main/java/org/example/spbootpr1/Spbootpr1Application.java
+++ src/main/java/org/example/spbootpr1/Spbootpr1Application.java
@@ -3,14 +3,35 @@ package org.example.spbootpr1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 @SpringBootApplication
+@EnableScheduling
 @EnableAsync
 public class Spbootpr1Application {
 
     
     public static void main(String[] args) {
+
+//
+//        class ClockTask extends TimerTask {
+//            private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+//            @Override
+//            public void run() {
+//                System.out.println("\r" + "Time is: " + dateFormat.format(new Date()));
+//            }
+//        }
+//
+//
+//        Timer timer = new Timer();
+//        timer.scheduleAtFixedRate(new ClockTask(),5000,1000);
+
         SpringApplication.run(Spbootpr1Application.class, args);
     }
 
